66365bcb1db92608ad4de939646d44d20174ba0a
Kripchek <133848904+Kripchek@users.noreply.github.com>
2023-05-17 12:05:32 +0300

    Добавлены скиллы (в разработке) и сделана элементарная боевка

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,13 +19,7 @@ namespace ConsoleApp1
             Console.WriteLine(c * d);
             Console.WriteLine(c / d);
             Console.WriteLine(c - d);
-            Console.WriteLine("Введите ваше имя");
-            string e = Console.ReadLine();
-            Console.WriteLine("Введите вашу фамилию");
-            string f = Console.ReadLine();
-            Console.WriteLine("Введите ваше Отчество");
-            string g = Console.ReadLine();
-            Console.WriteLine("Вы ввели: "+ e + " " + f + " " + g);
+          
             float p = 40;
             float pdmg = 16;
             float pdef = 12;
@@ -36,19 +30,62 @@ namespace ConsoleApp1
             float toMdmg = pdmg / mdef;
             float Php = p - toPdmg;
             float Mhp = m - toMdmg;
-            Console.WriteLine("Здоровье игрока: " + p);
-            Console.WriteLine("Урон игрока: " + pdmg);
-            Console.WriteLine("Защита игрока: " + pdef);
-            Console.WriteLine("Здоровье монстра: " + m);
-            Console.WriteLine("Урон монстра: " + mdmg);
-            Console.WriteLine("Защита монстра: " + mdef);
+          
 
+            float Playerhp = 40;
+            float Monsterhp = 25;
+                Console.WriteLine("суммарный урон по игроку");
+            float sumPDmg = toPdmg;
+            Console.WriteLine(sumPDmg);
+                Console.WriteLine("суммарный урон по монстру");
+            float sumMDmg = toMdmg;
+            Console.WriteLine(sumMDmg);
+            
+            Console.WriteLine("Начало боя");
+            while (Playerhp > 0 && Monsterhp > 0)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("Выберите навык который хотие использовать:1 - Fireball; 2 - Shadow Dance; 3 - First Strike;" +
+                " 4 - Reverse Polarity; 5 - Charge of Darkness");
+                int mode = int.Parse(Console.ReadLine());
+                switch (mode)
+                {
+                    case 1:
+                        Console.WriteLine("вы выбрали fireball");
+                        break;
+                    case 2:
+                        Console.WriteLine("вы выбрали shadow dance");
+                        break;
+                    case 3:
+                        Console.WriteLine("вы выбрали first strike");
+                        break;
+                    case 4:
+                        Console.WriteLine("вы выбрали reverse polarity");
+                        break;
+                    case 5:
+                        Console.WriteLine("вы выбрали charge of darkness");
+                        break;
+                }
+                Console.WriteLine("Здоровье игрока");
+                    Playerhp -= toPdmg;
+                Console.WriteLine(Playerhp);
+
+                Console.WriteLine("Здоровье Монстров");
+                    Monsterhp -= toMdmg;
+                    Console.WriteLine(Monsterhp);
+                Console.WriteLine("суммарный урон по монстру");
+                    sumMDmg +=  toMdmg;
+                Console.WriteLine(sumMDmg);
+                Console.WriteLine("суммарный урон по игроку");
+                    sumPDmg +=  toPdmg;
+                Console.WriteLine(sumPDmg);
+                Console.WriteLine("==================================");
+            }
             Console.WriteLine("Итоги битвы:");
-            Console.WriteLine("Здоровье оставшееся у Игрока: " + Php);
-            Console.WriteLine("Здоровье оставшееся у Монстра " + Mhp);
-            Console.WriteLine("Урон нанесеный Игроком: " + toMdmg);
-            Console.WriteLine("Урон нанесеный Монстром: " + toPdmg);
-
+            Console.WriteLine("Здоровье оставшееся у Игрока: " + Playerhp);
+            Console.WriteLine("Здоровье оставшееся у Монстра " + Monsterhp);
+            Console.WriteLine("Урон нанесеный Игроком: " + sumMDmg);
+            Console.WriteLine("Урон нанесеный Монстром: " + sumPDmg);
             Console.ReadKey();
         }
     }
